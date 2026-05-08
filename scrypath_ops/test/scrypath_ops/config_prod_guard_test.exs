58c948707d1a13e9362c9feb3b7fa4589e371fff
szTheory <szTheory@users.noreply.github.com>
2026-05-08 11:28:38 -0400

    fix: align opsui auth mode contract test

--- scrypath_ops/test/scrypath_ops/config_prod_guard_test.exs
+++ scrypath_ops/test/scrypath_ops/config_prod_guard_test.exs
@@ -1,7 +1,7 @@
 defmodule ScrypathOps.ConfigProdGuardTest do
   use ExUnit.Case, async: true
 
-  @modes ~w(basic proxy_headers)
+  @modes ~w(basic proxy_headers sigra)
 
   test "prod requires OPSUI_AUTH_MODE documentation constant" do
     assert ScrypathOps.Security.allowed_opsui_auth_modes() == @modes
@@ -10,6 +10,6 @@ defmodule ScrypathOps.ConfigProdGuardTest do
 
   test "contract uses Enum.member? for OPSUI_AUTH_MODE allow-list" do
     mode = hd(@modes)
-    assert Enum.member?(["basic", "proxy_headers"], mode)
+    assert Enum.member?(["basic", "proxy_headers", "sigra"], mode)
   end
 end
