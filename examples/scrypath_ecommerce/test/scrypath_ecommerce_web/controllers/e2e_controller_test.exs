51fc6a3aec057b33d6cf31fdbb20b4da02edd0bc
szTheory <szTheory@users.noreply.github.com>
2026-05-30 15:25:07 -0400

    test(103-00): add e2e seed controller test stubs

---
+++ examples/scrypath_ecommerce/test/scrypath_ecommerce_web/controllers/e2e_controller_test.exs
@@ -0,0 +1,10 @@
+defmodule ScrypathEcommerceWeb.E2EControllerTest do
+  use ScrypathEcommerceWeb.ConnCase
+
+  describe "E2E seed endpoint" do
+    @tag :skip
+    test "seed/2 dynamically invokes catalog fixtures", %{conn: _conn} do
+      flunk("Not implemented: Should seed data for E2E tests using fixtures")
+    end
+  end
+end
